3ae9d512962ad65bad5829e4e10a8c04f61c1c55
ibbolia <ibbolia@gmail.com>
2014-09-08 20:04:52 -0400

    First Commit. Adds a changelog and the first tool.

---
+++ IsPrime.py
@@ -0,0 +1,15 @@
+def isPrime(num):
+	#a brute force attempt to determine if a number is prime
+	#basically divide by each number from 2 to num-1. If at any point num%div = 0, return false, else true
+	#additionally, this function assumes that negative numbers and 0 are not prime.
+	#Mathematically, the loop should also allow 1 to comfirm as a prime number, since it will skip the loop to the return True statement
+	#Efficiency is probably around worst O(num)
+	
+	if  num < 1:
+		return False
+	i = 2
+	while i < num-1:
+		if num%i == 0:
+			return False
+		i += 1
+	return True
